834639f71cbef7b62a3dfea272d21a6a46ae8094
shajn786 <shajn786@gmail.com>
2022-11-21 10:56:17 +0530

    multiplication

--- src/Main.java
+++ src/Main.java
@@ -34,6 +34,11 @@ public class Main {
                     result = num1 - num2;
                     System.out.println("Result is" + result);
 
+                case "*":
+
+                    result = num1  * num2;
+                    System.out.println("Result is" + result);
+
             }
 
 
