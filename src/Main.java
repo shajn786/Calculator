555ef4c9dde9fe0d53eeaf437b81eab8354f9f2c
shajn786 <shajn786@gmail.com>
2022-11-21 10:40:59 +0530

    addition

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,40 @@
+import java.util.Scanner;
+
 public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+    static int num1,num2,result,val;
+    static String operator =null;
+    static boolean flag;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+
+        while(true) {
+            System.out.println("enter the operator");
+            System.out.println("+ : To addition\n- : subtraction\n * : To multiplication\n / : To division\n enter 0 to exit ");
+            operator = sc.next();
+            System.out.println("enter the first number");
+            num1 = sc.nextInt();
+            System.out.println("enter the Second number");
+            num2 = sc.nextInt();
+            System.out.println();
+
+
+
+
+
+            switch (operator) {
+                case "+":
+
+                    result = num1 + num2;
+                    System.out.println("Result is" + result);
+
+            }
+
+
+        }
+
+
+
     }
 }
